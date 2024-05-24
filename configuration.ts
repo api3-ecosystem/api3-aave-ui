f76655919c05268974a04cd52c1bb1e6700bf1a7
Aamir Alam <aamiralam1991@gmail.com>
2024-05-24 22:56:34 +0530

    fix configs

--- configuration.ts
+++ configuration.ts
@@ -1,21 +1,20 @@
-import defaultConfig from "default-configs.json";
+import config from "deployment-configs.json";
 
 import { zeroAddress } from "viem";
-import { existsSync, readFileSync } from "fs";
+
+const deploymentConfig: any = config;
 
 export const populateChainsToConfiure = () => {
   try {
     let chainId;
 
-    if (existsSync("generated-config/config.json")) {
-      console.log("using cli chains");
-      // read config from cli
-      const configRaw = readFileSync("generated-config/config.json", "utf-8");
-      const config = JSON.parse(configRaw);
-      chainId = config.network.chainId;
-    } else {
+    if (!deploymentConfig?.generated?.config) {
       console.log("using default chains");
-      chainId = defaultConfig.config.network.chainId;
+      chainId = deploymentConfig.default.config.network.chainId;
+    } else {
+      console.log("using generated chains");
+      // read config from cli
+      chainId = deploymentConfig.generated.config.network.chainId;
     }
 
     console.log({ chainId });
@@ -30,19 +29,15 @@ export const populateChainsToConfiure = () => {
 export const populateMarket = () => {
   let contracts;
 
-  if (existsSync("generated-config/deployed-contracts.json")) {
+  if (!deploymentConfig?.generated?.["deployed-contracts"]) {
+    console.log("using default contracts");
+    contracts = deploymentConfig.default?.["deployed-contracts"];
+  } else {
     console.log("using cli contracts");
 
     // read config from cli
-    const raw = readFileSync(
-      "generated-config/deployed-contracts.json",
-      "utf-8",
-    );
-    contracts = JSON.parse(raw);
-    // chainId = config.network.chainId;
-  } else {
-    console.log("using default contracts");
-    contracts = defaultConfig["deployed-contracts"];
+
+    contracts = deploymentConfig?.generated?.["deployed-contracts"];
   }
 
   const market = {
